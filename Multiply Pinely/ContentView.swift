39be76ecd5d57d1ccba83a9ae22e6cb8033bdec8
Bern <bernnystrom@gmail.com>
2022-06-17 07:14:32 +0800

    🚧 (chore): Add new method to generate list of random question
    Added a new Question struct for storing the question text and answer generated from the generateQuestion method. Based on the user selected options in-game on the number of questions, the method will also append the Question struct to a @State property questionsList.

--- Multiply Pinely/ContentView.swift
+++ Multiply Pinely/ContentView.swift
@@ -7,23 +7,58 @@
 
 import SwiftUI
 
+struct Question {
+    var text: String
+    var answer: Int
+}
+
 struct ContentView: View {
     @State private var multiplyNumber = 2
     @State private var numberOfQuestions = 5
+    @State private var questionsList = [Question]()
     @State private var settingGameRound = true
     @State private var gameRoundActive = false
     
     var body: some View {
-        List {
-            Section("Select Your Multiplication Table") {
-                Stepper(" Table of \(multiplyNumber)", value: $multiplyNumber, in: 2...12, step: 1)
+        ZStack {
+            List {
+                Section("Select Your Multiplication Table") {
+                    Stepper(" Table of \(multiplyNumber)", value: $multiplyNumber, in: 2...12, step: 1)
+                }
+                
+                Section("Desired Number of Questions (Max. 20)") {
+                    Stepper("\(numberOfQuestions)", value: $numberOfQuestions, in: 5...20, step: 5)
+                }
+            }
+            .disabled(settingGameRound ? false : true)
+                
+            VStack {
+                Section {
+                    Text(gameRoundActive ? questionsList[0].text : "")
+                }
             }
             
-            Section("Desired Number of Questions (Max. 20)") {
-                Stepper("\(numberOfQuestions)", value: $numberOfQuestions, in: 5...20, step: 5)
+            HStack {
+                Button {
+                    generateQuestion()
+                } label: {
+                    Text("Start Game")
+                }
+                .padding()
+                .background(.mint)
+                .foregroundColor(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
             }
         }
-        .disabled(settingGameRound ? false : true)
+    }
+    
+    func generateQuestion() {
+        let randomNumber = Int.random(in: 1...12)
+        let question = Question(text: "What is \(randomNumber) x \(multiplyNumber)", answer: randomNumber * multiplyNumber)
+        
+        for _ in 0..<numberOfQuestions {
+            questionsList.append(question)
+        }
     }
 }
 
